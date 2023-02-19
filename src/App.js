014842dec58320db031cfd86ab550611a1f32f43
salini4kamal <salini4kaml@gmail.com>
2023-02-19 09:09:19 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       
+      </header>
     </div>
   );
 }
